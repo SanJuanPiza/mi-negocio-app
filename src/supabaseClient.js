f53d3469dea75f5809a8612aa09cbf463032ee07
SanJuanPiza <crissp157@gmail.com>
2025-10-29 22:20:39 -0600

    Oculta las llaves de Supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// REEMPLAZA CON TU URL
-const supabaseUrl = 'https://wkyxozsgthszpeiqbhav.supabase.co'
-// REEMPLAZA CON TU API KEY (anon public)
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndreXhvenNndGhzenBlaXFiaGF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3NzI1OTcsImV4cCI6MjA3NzM0ODU5N30.2OTq5drz0h7qSC1FMppIPYgyKiMwcdz465zbWPIMmNg'
+// El código ahora las lee de Vercel (import.meta.env)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
